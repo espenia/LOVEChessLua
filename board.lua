feb878ea8129a6942a8b828475a28e6dac2f0ef5
fedeboco <fedeboco@gmail.com>
2021-01-12 20:26:46 -0300

    pressed piece drawed on top

--- board.lua
+++ board.lua
@@ -12,17 +12,44 @@ Board = Object:extend()
 function Board:new()
     self.gridSize = 100
     self.pieces = self:Pieces()
+    self.pressed = -1 --index of pressed piece. -1 == none pressed
 end
 
 function Board:update(dt)
+    if self.pressed ~= -1 then
+        self:updatePressed()
+    end
+    if self.pressed == -1 then
+        self:updateAll()
+    end
+end
+
+function Board:updatePressed()
+    self.pieces[self.pressed]:update()
+    if self.pieces[self.pressed].clicked == false then
+        self.pressed = -1
+    end
+end
+
+function Board:updateAll()
     for i = 1, #self.pieces do
         self.pieces[i]:update()
+        if self.pieces[i].clicked then
+            self.pressed = i
+            break
+        end
     end
 end
 
 function Board:draw()
     for i = 1, #self.pieces do
-        self.pieces[i]:draw()
+        if self.pressed ~= i then
+            self.pieces[i]:draw()
+        end
+    end
+    -- draws pressed piece on top
+    if self.pressed ~= -1 then
+        self.pieces[self.pressed]:draw()
     end
 end
 
@@ -43,4 +70,7 @@ function Board:WhitePieces(pieces)
     table.insert(pieces, Rook("w", boardEnd - step, 20))
     table.insert(pieces, Queen("w", 3 * step, 20))
     table.insert(pieces, King("w", boardEnd - 4  * step, 20))
+    for i = 0, 7 do
+        table.insert(pieces, Pawn("w", i * step, 120))
+    end
 end
