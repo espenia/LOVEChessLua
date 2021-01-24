eff1122f90e25723a4b83f26cabb1c30613f229c
Raul <chaparroraulantonio@gmail.com>
2021-01-24 00:58:11 -0300

    Agrego logica de enroque

--- Pieces/king.lua
+++ Pieces/king.lua
@@ -9,6 +9,8 @@ function King:new(color, x, y, gridSize, xOffset, yOffset, posX, posY)
     end
     self.super.new(self, color, x, y, gridSize, xOffset, yOffset, posX, posY)
     self.name = "king"
+    self.possibleCastling = true
+    self.castlingSide = 0
 end
 
 
@@ -17,10 +19,10 @@ function King:color()
 end
 
 function King:validateMovement(movement)
-    xo,yo = movement:getStart()
-    xf,yf = movement:getEnd()
-    deltaX = xo - xf
-    deltaY = yo - yf
+    local xo,yo = movement:getStart()
+    local xf,yf = movement:getEnd()
+    local deltaX = xo - xf
+    local deltaY = yo - yf
     if  (deltaX == 1 and deltaY == 0) or
         (deltaX == - 1 and deltaY == 0) or 
         (deltaX == 0 and deltaY == 1) or
@@ -29,12 +31,57 @@ function King:validateMovement(movement)
         (deltaX == 1 and deltaY == -1) or
         (deltaX == -1 and deltaY == 1) or
         (deltaX == -1 and deltaY == -1) then
+        self.possibleCastling = false
         return true
-    else
+    elseif ((deltaX == 2 or deltaX == -2) and deltaY == 0) and
+            self.possibleCastling == true then
+        --if this:kingCastling( deltaX ) then
+            self.castlingInProcess = true
+            if deltaX == -2 then
+                self.castlingSide = 1
+            else
+                self.castlingSide = -1
+            end
+            return true       
+    else 
         return false
     end
 end  
 
 function King:checkTrajectory( x, y, xf, yf, xo,yo)
-    return false;
+    if self.castlingInProcess == true then
+        if  (xo < x and x < xf and y == yo) or
+            (xf < x and x < xo and y == yo) then
+            return true
+        else
+            return false
+        end
+    end
+end
+
+function King:checkPossibleCasteling(pieces, board, size)
+    
+    if self.castlingInProcess == true then
+        for i = 1, size do
+            if pieces[i]:getName() == "rook" then
+                local x,y,castelingOK = pieces[i]:checkRookCasteling(self.castlingSide)
+                if castelingOK then
+                    local xo,yo = pieces[i]:getActualPos()
+                    for j = 1, size do
+                        local xp,yp = pieces[j]:getActualPos()
+                            if pieces[i]:checkTrajectory(xp, yp, x, y, xo, yo) == true and pieces[j]:getName() ~= "king" then
+                                return false     
+                            end
+                    end
+                    pieces[i]:move(x,y)
+                    return true   
+                    --pieces[i]:setClicked()
+                    
+                    --board.lastMove:endMove(x,y)
+                    -- return true
+                end    
+            end
+        end
+    end
+    return false
 end
